dd1bef50e990bef41d0dc5083d920a4aa029444d
Davila <daniela.avila.franco.01@gmail.com>
2017-08-01 00:02:17 -0500

    Commit 01: Configuracion inicial

--- app.js
+++ app.js
@@ -3,13 +3,24 @@ var express = require('express');
 
 var app = express();
 var port = process.env.PORT || 5000;
+var nav = 
+[
+    {
+        Text: "Reportes",
+        Link: "/Reportes"
+    },
+    {
+        Text:"Estaciones",
+        Link:"/Estaciones"
+    }
+];
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 
 //TEMPLAN ENGINE
-app.set('views','./src/views');
-app.set('view engine','ejs');
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 
 //ROOT
@@ -17,7 +28,7 @@ app.get('/', function (req, res) {
 
     res.render('index', {
         title: 'Inicio',
-        nav: [{}]
+        nav: nav
     }
     );
 
